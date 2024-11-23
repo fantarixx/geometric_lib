d5904e9f0fb640f6e1a00ac634d2b52613285eb7
Roman <romanplatonov30@mail.ru>
2024-11-23 18:02:41 +0300

    fixed mistakes with tests v0.5

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -10,30 +10,25 @@ class TestCircle(unittest.TestCase):
         with self.assertRaises(ValueError):
             perimeter(r)
 
-
     def test2(self):
         r = -52
         with self.assertRaises(ValueError):
             area(r)
-
 
     def test3(self):
         r = 0
         correct = 0
         self.assertEqual(area(r), correct)
 
-
     def test4(self):
         r = 0
         correct = 0
         self.assertEqual(perimeter(r), correct)
 
-
     def test5(self):
         r = 52
         correct = math.pi * r * r
         self.assertAlmostEqual(area(r), correct)
-
 
     def test6(self):
         r = 52
